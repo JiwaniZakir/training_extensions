e3fe4ef16a800a0c49d526766168916ac06b831e
Joao Vilaca <joao.vilaca@intel.com>
2026-03-23 09:36:48 +0100

    Update rsbuild

--- application/ui/rsbuild.config.ts
+++ application/ui/rsbuild.config.ts
@@ -7,7 +7,15 @@ import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSass } from '@rsbuild/plugin-sass';
 import { pluginSvgr } from '@rsbuild/plugin-svgr';
 
-const { publicVars } = loadEnv({ prefixes: ['PUBLIC_'] });
+const { publicVars } = loadEnv();
+
+const getPublicApiUrl = () => {
+    if (publicVars['import.meta.env.PUBLIC_API_BASE_URL'] !== undefined) {
+        return JSON.parse(publicVars['import.meta.env.PUBLIC_API_BASE_URL']);
+    }
+
+    return '';
+};
 
 export default defineConfig({
     plugins: [
@@ -35,10 +43,8 @@ export default defineConfig({
     source: {
         define: {
             ...publicVars,
-            'import.meta.env.PUBLIC_API_BASE_URL':
-                publicVars['import.meta.env.PUBLIC_API_BASE_URL'] ?? '"http://localhost:7860"',
-            'process.env.PUBLIC_API_BASE_URL':
-                publicVars['process.env.PUBLIC_API_BASE_URL'] ?? '"http://localhost:7860"',
+            'import.meta.env.PUBLIC_API_BASE_URL': publicVars['import.meta.env.PUBLIC_API_BASE_URL'] ?? '""',
+            'process.env.PUBLIC_API_BASE_URL': publicVars['import.meta.env.PUBLIC_API_BASE_URL'] ?? '""',
             // Needed to prevent an issue with spectrum's picker
             // eslint-disable-next-line max-len
             // https://github.com/adobe/react-spectrum/blob/6173beb4dad153aef74fc81575fd97f8afcf6cb3/packages/%40react-spectrum/overlays/src/OpenTransition.tsx#L40
@@ -77,9 +83,9 @@ export default defineConfig({
                 "default-src 'self'; " +
                 "script-src 'self' 'unsafe-eval' blob:; " +
                 "worker-src 'self' blob:; " +
-                "connect-src 'self' http://localhost:7860 data:; " +
-                "img-src 'self' http://localhost:7860 data: blob:; " +
-                "media-src 'self' http://localhost:7860 blob: data:; " +
+                `connect-src 'self' ${getPublicApiUrl()} data:; ` +
+                `img-src 'self' ${getPublicApiUrl()} data: blob:; ` +
+                `media-src 'self' ${getPublicApiUrl()} blob: data:; ` +
                 "style-src 'self' 'unsafe-inline';",
         },
     },
